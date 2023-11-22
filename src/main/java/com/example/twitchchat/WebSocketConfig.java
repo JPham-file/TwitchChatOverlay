ff73f92c50c4171ef47fdbf3c1ce83275cae3a07
JP Pham <63124165+JPham-file@users.noreply.github.com>
2023-11-22 17:39:43 -0600

    not hardcoded address anymore

--- src/main/java/com/example/twitchchat/WebSocketConfig.java
+++ src/main/java/com/example/twitchchat/WebSocketConfig.java
@@ -6,12 +6,25 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.net.UnknownHostException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     @Autowired
     private WebSocketChatHandler webSocketChatHandler;
+    private String myIPAddress;
+
+    public WebSocketConfig() {
+        try {
+            myIPAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException a) {
+            a.printStackTrace();
+        }
+    }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(webSocketChatHandler, "/ws/chat").setAllowedOrigins("*");
