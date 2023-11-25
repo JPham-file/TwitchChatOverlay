75a170edfad93f44dc06576740fe6d9abc40440b
JP Pham <63124165+JPham-file@users.noreply.github.com>
2023-11-25 14:47:38 -0600

    refactored renderMessage.js

--- frontend/renderMessage.js
+++ frontend/renderMessage.js
@@ -1,61 +1,71 @@
-import {StyleSheet, Text, View} from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View, Image } from "react-native";
 import emotes from './emote';
-import {Image} from "react-native";
 
-const renderMessage = (message, color) => {
+const renderMessage = (message, getColor) => {
     const messageComponents = [];
     const words = message.split(' ');
     let isFirstWord = true;
-    let firstWordColor = color();
+    const firstWordColor = getColor();
 
     words.forEach((word, index) => {
         if (emotes[word]) {
-            // If the word is an emote, render an Image
+            if (isFirstWord) {
+                // If it's the first word and an emote, reset the flag
+                isFirstWord = false;
+            }
             messageComponents.push(
                 <Image
                     key={`emote-${index}`}
                     source={emotes[word]}
-                    style={{width: 35, height: 35}}
+                    style={styles.emoteStyle}
                     resizeMode="contain"
                 />
-
             );
+
+            // Add a space after the emote
             messageComponents.push(
-                <Text key={`text-${index}`}>
+                <Text key={`space-${index}`}>
                     {' '}
                 </Text>
             );
+
         } else {
-            const textStyle = isFirstWord ?
-                {...styles.messageText, fontWeight: 'bold', color: firstWordColor} :
-                styles.messageText;
+            const wordStyle = isFirstWord
+                ? { ...styles.firstWordText, color: firstWordColor }
+                : styles.messageText;
 
             messageComponents.push(
-                <Text key={`text-${index}`} style={textStyle}>
+                <Text key={`text-${index}`} style={wordStyle}>
                     {word + ' '}
                 </Text>
             );
-        }
 
-        // Ensure only the first word is styled differently
-        isFirstWord = false;
+            // Reset the flag after the first word
+            isFirstWord = false;
+        }
     });
 
-    return <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>{messageComponents}</View>;
+    return <View style={styles.messageContainer}>{messageComponents}</View>;
 };
+
 const styles = StyleSheet.create({
-    container: {
-        padding: 12,
-    },
-    message: {
-        padding: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#000000',
-        marginVertical: 1,
+    messageContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
     },
     messageText: {
-        fontSize: 18,
+        fontSize: 16,
         color: 'white',
+    },
+    firstWordText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        // color is set dynamically
+    },
+    emoteStyle: {
+        width: 35,
+        height: 35,
     },
 });
 
